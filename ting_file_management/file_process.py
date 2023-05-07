a86886f8bbfe99435b38f1315edcca46342cc55e
Manuela França <manuelaalves26@gmail.com>
2023-05-07 16:50:42 -0300

    feat: implementado função process que transforma o conteúdo da lista gerada pela função txt_importer em um dicionário.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,26 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    process_exist = False
+    for index in range(len(instance)):
+        file = instance.search(index)
+        if file["nome_do_arquivo"] == path_file:
+            process_exist = not process_exist
+            break
+
+    if not process_exist:
+        content = txt_importer(path_file)
+
+        file_dict = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(content),
+            "linhas_do_arquivo": content,
+        }
+
+        instance.enqueue(file_dict)
+        return sys.stdout.write(f"{file_dict}\n")
 
 
 def remove(instance):
